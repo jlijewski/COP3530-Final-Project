f24ecd1c93d2cd60152ffb22843802e9f38f2fa7
jlijewski <80366830+jlijewski@users.noreply.github.com>
2022-04-18 11:15:54 -0400

    Update maxHeap.h

--- maxHeap.h
+++ maxHeap.h
@@ -11,6 +11,8 @@
 #include <assert.h>
 #include <bits/stdc++.h>
 #include <math.h>
+
+
 using namespace std;
 
 
@@ -44,48 +46,9 @@ class maxHeap{
     vector<Movie> heap;
    public:
 
-    void insertMovie(string title, string year, string genre, double rating, double tenRating, int movieVotes, string runTime){
-	 //maxHeap * newNode = new Movie( title, year, genre, rating,tenRating, movieVotes, runTime);
-	  //heap.push_back(newNode);
-      heapifyup(heap.size() -1);
-
-    }
-
-    void heapifyUp(int rating){
-
-    if (in >= 0 && par(rating) >= 0 && heap[par(rating)] > heap[rating]) {
-          int temp = heap[rating];
-          heap[rating] = heap[par(rating)];
-          heap[par(rating)] = temp;
-          heapifyup(par(rating));
-       }
-
-
-    }
-
-
-    void heapifyDown(int rating){
-
-    int child = l(rating);
-
-       int child1 = r(rating);
-
-       if (child >= 0 && child1 >= 0 && heap[child] > heap[child1]) {
-          child = child1;
-       }
-
-       if (child > 0 && heap[rating] > heap[child]) {
-
-          int t = heap[rating];
-          heap[rating] = heap[child];
-          heap[child] = t;
-          heapifydown(child);
-
-       }
-
-
-
-    };
+    void insertMovie(string title, string year, string genre, double rating, double tenRating, int movieVotes, string runTime);
+    void heapifyUp(int index);
+    void heapifyDown(int index);
     void getMax();
     void deleteMax();
     
